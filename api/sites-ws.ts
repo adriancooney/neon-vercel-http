1128d8bcc2a46a62e345b1616c94932517f46234
Adrian Cooney <cooney.adrian@gmail.com>
2023-06-01 09:58:08 +0100

    compare

---
+++ api/sites-ws.ts
@@ -0,0 +1,26 @@
+import { sql as db } from "@vercel/postgres";
+
+export const config = {
+  runtime: "edge",
+  regions: ["fra1"], // fra1 = Frankfurt: pick the Vercel region nearest your Neon DB
+};
+
+export default async (req: Request, ctx: any) => {
+  const longitude = parseFloat(
+    req.headers.get("x-vercel-ip-longitude") ?? "-122.47"
+  );
+  const latitude = parseFloat(
+    req.headers.get("x-vercel-ip-latitude") ?? "37.81"
+  );
+
+  const sites = await db`
+    SELECT
+      id_no, name_en, category,
+      'https://whc.unesco.org/en/list/' || id_no || '/' AS link,
+      location <-> st_makepoint(${longitude}, ${latitude}) AS distance
+    FROM whc_sites_2021
+    ORDER BY distance
+    LIMIT 10`;
+
+  return new Response(JSON.stringify({ longitude, latitude, sites }, null, 2));
+};
